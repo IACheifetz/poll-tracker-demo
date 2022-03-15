0757abf1291297c6cf826dcd851575d06e8c2097
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-15 16:03:58 -0700

    set up HTML elements and grabbed them in app js

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 // import functions and grab DOM elements
+const optionAAddButton = document.querySelector('#option-1-add');
+const optionBAddButton = document.querySelector('#option-2-add');
+const optionAUndoButton = document.querySelector('#option-1-undo');
+const optionBUndoButton = document.querySelector('#option-2-undo');
 
+const closePollButton = document.querySelector('#close-poll');
+const questionEl = document.querySelector('#poll-question');
+const optionATitleEl = document.querySelector('#option-1-title');
+const optionBTitleEl = document.querySelector('#option-2-title');
+const optionAVotesEl = document.querySelector('#option-1-votes');
+const optionBVotesEl = document.querySelector('#option-2-votes');
+
+const pastPollsEl = document.querySelector('.past-polls');
 // let state
+let question = '';
+let option1Votes = 0;
+let option2Votes = 0;
+let option1Title = '';
+let option2Title = '';
 
+const pastPollsArray = [];
 // set event listeners 
   // get user input
   // use user input to update state 
